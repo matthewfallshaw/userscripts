bcc8e6532e9d49c78b7c6f13957a78bb00a66d95
Matthew Fallshaw <m@fallshaw.me>
2021-05-09 21:41:57 +1000

    Update Hammerspoon redirector.user.js
    Remove apps I no longer use.

--- HammerspoonRedirector/Hammerspoon redirector.user.js
+++ HammerspoonRedirector/Hammerspoon redirector.user.js
@@ -2,7 +2,7 @@
 // @name         Hammerspoon redirector
 // @namespace    http://fallshaw.me/
 // @description  Open SSB links in SSB (Epichrome or Fluid) apps
-// @version      0.6
+// @version      0.7
 // @homepage     https://github.com/matthewfallshaw/userscripts/blob/master/HammerspoonRedirector/Hammerspoon%20redirector.user.js
 // @updateURL    https://github.com/matthewfallshaw/userscripts/raw/master/HammerspoonRedirector/Hammerspoon%20redirector.user.js
 // @downloadURL  https://github.com/matthewfallshaw/userscripts/raw/master/HammerspoonRedirector/Hammerspoon%20redirector.user.js
@@ -10,14 +10,7 @@
 // @contributionURL https://intelligence.org/donate/
 // @author       Matthew Fallshaw
 // @copyright    https://creativecommons.org/licenses/by/4.0/
-// @match        *://www.pivotaltracker.com/n/*
-// @match        *://www.pivotaltracker.com/story/*
-// @match        *://www.pivotaltracker.com/dashboard
-// @match        *://www.pivotaltracker.com/reports/*
-// @match        *://www.pivotaltracker.com/projects/*
-// @match        *://www.pivotaltracker.com/epic/*
 // @match        *://app.asana.com/*
-// @match        *://morty.trikeapps.com/*
 // @match        *://www.google.com/calendar/*
 // @match        *://calendar.google.com/*
 // @match        *://meet.google.com/*
