13421dfbf7874392b8e0ed15f25b924db542e037
Matthew Fallshaw <m@fallshaw.me>
2023-06-19 17:00:18 +1000

    Update Hammerspoon redirector.user.js
    `@exclude` wildcards don't operate on query parameters. Check for those in the code.

--- HammerspoonRedirector/Hammerspoon redirector.user.js
+++ HammerspoonRedirector/Hammerspoon redirector.user.js
@@ -7,7 +7,6 @@
 // @updateURL    https://github.com/matthewfallshaw/userscripts/raw/master/HammerspoonRedirector/Hammerspoon%20redirector.user.js
 // @downloadURL  https://github.com/matthewfallshaw/userscripts/raw/master/HammerspoonRedirector/Hammerspoon%20redirector.user.js
 // @supportURL   https://github.com/matthewfallshaw/userscripts/issues
-// @contributionURL https://intelligence.org/donate/
 // @author       Matthew Fallshaw
 // @copyright    https://creativecommons.org/licenses/by/4.0/
 // @match        *://app.asana.com/*
@@ -15,8 +14,6 @@
 // @match        *://www.google.com/calendar/*
 // @match        *://calendar.google.com/*
 // @match        *://meet.google.com/*
-// @exclude      *://meet.google.com/call?*origin=https://mail.google.com/*
-// @exclude      *://meet.google.com/_/frame?*origin=https://mail.google.com/*
 // @grant        window.close
 // @run-at       document-start
 // ==/UserScript==
@@ -27,6 +24,14 @@
 // ----
 
 (function() {
-  window.open("hammerspoon://URLDispatcher?uri=" + encodeURIComponent(window.location.href), "_self");
-  window.close();
+  const url = new URL(window.location.href);
+  const origin = url.searchParams.get('origin');
+  const pathname = url.pathname;
+
+  // Check if the 'origin' query parameter is 'https://mail.google.com' and
+  // pathname does not start with '/call' or '/_/frame'
+  if (origin !== 'https://mail.google.com' && !(pathname.startsWith('/call') || pathname.startsWith('/_/frame'))) {
+    window.open("hammerspoon://URLDispatcher?uri=" + encodeURIComponent(window.location.href), "_self");
+    window.close();
+  }
 })();
